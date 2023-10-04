29f3d545a9e97da4085b49cd0826f570758facc4
Pawel <pawelbernys11@gmail.com>
2023-10-04 09:19:35 +0200

    testowanie

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+
+        System.out.println(12+45);
+        int a = 12;
+        int b = 45;
+        System.out.println(a + b + " wynik");
+        System.out.println("wynik" + a + b );
+        System.out.println(b / a);
+        System.out.println((double)b/a);
+    }
+}
